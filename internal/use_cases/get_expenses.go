0213d87788ea160e567bec72ed77fe50b6850927
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-08-08 20:29:03 -0300

    feat: add GetExpensesUseCase

---
+++ internal/use_cases/get_expenses.go
@@ -0,0 +1,28 @@
+package usecases
+
+import (
+	"github.com/GuilhermeDeOliveiraAmorim/expense-tracker/internal/entities"
+	"github.com/GuilhermeDeOliveiraAmorim/expense-tracker/internal/repositories"
+)
+
+type GetExpensesInputDto struct {
+}
+
+type GetExpensesOutputDto struct {
+	Expenses []entities.Expense `json:"categories"`
+}
+
+type GetExpensesUseCase struct {
+	ExpenseRepository repositories.ExpenseRepositoryInterface
+}
+
+func (c *GetExpensesUseCase) Execute(input GetExpensesInputDto) (GetExpensesOutputDto, []error) {
+	searchedsExpenses, err := c.ExpenseRepository.GetExpenses()
+	if err != nil {
+		return GetExpensesOutputDto{}, err
+	}
+
+	return GetExpensesOutputDto{
+		Expenses: searchedsExpenses,
+	}, nil
+}
