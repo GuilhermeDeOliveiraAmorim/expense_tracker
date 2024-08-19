ead4c07f706ef4a90692679767a4347864dbfa19
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-08-19 15:56:17 -0300

    feat: adds db configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/GuilhermeDeOliveiraAmorim/expense-tracker/configs"
+	repositoriesgorm "github.com/GuilhermeDeOliveiraAmorim/expense-tracker/internal/infra/repositories_gorm"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	dsn := "host=" + configs.PostgresServer + " user=" + configs.PostgresUser + " password=" + configs.PostgresPassword + " dbname=" + configs.PostgresDb + " port=" + configs.PostgresPort + " sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	if err := db.AutoMigrate(
+		repositoriesgorm.Categories{},
+		repositoriesgorm.Expenses{},
+		repositoriesgorm.Users{},
+	); err != nil {
+		fmt.Println("Erro durante a migração:", err)
+		return
+	}
+	fmt.Println("Migração bem-sucedida!")
+}
