dc419788c930f7ef5fe0e8961f431b99676d04e7
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-08-19 17:05:30 -0300

    feat: adds category handlers

---
+++ internal/interface/handlers/category_handler.go
@@ -0,0 +1,88 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/GuilhermeDeOliveiraAmorim/expense-tracker/internal/infra/factory"
+	usecases "github.com/GuilhermeDeOliveiraAmorim/expense-tracker/internal/use_cases"
+	"github.com/gin-gonic/gin"
+)
+
+type CategoryHandler struct {
+	categoryFactory *factory.CategoryFactory
+}
+
+func NewCategoryHandler(factory *factory.CategoryFactory) *CategoryHandler {
+	return &CategoryHandler{
+		categoryFactory: factory,
+	}
+}
+
+func (h *CategoryHandler) CreateCategory(c *gin.Context) {
+	var input usecases.CreateCategoryInputDto
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	output, err := h.categoryFactory.CreateCategory.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	c.JSON(http.StatusCreated, output)
+}
+
+func (h *CategoryHandler) GetCategory(c *gin.Context) {
+	var input usecases.GetCategoryInputDto
+	input.CategoryID = c.Param("id")
+
+	output, err := h.categoryFactory.GetCategory.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	c.JSON(http.StatusOK, output)
+}
+
+func (h *CategoryHandler) GetCategories(c *gin.Context) {
+	var input usecases.GetCategoriesInputDto
+	output, err := h.categoryFactory.GetCategories.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	c.JSON(http.StatusOK, output)
+}
+
+func (h *CategoryHandler) UpdateCategory(c *gin.Context) {
+	var input usecases.UpdateCategoryInputDto
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	output, err := h.categoryFactory.UpdateCategory.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	c.JSON(http.StatusOK, output)
+}
+
+func (h *CategoryHandler) DeleteCategory(c *gin.Context) {
+	var input usecases.DeleteCategoryInputDto
+	input.CategoryID = c.Param("id")
+
+	output, err := h.categoryFactory.DeleteCategory.Execute(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	c.JSON(http.StatusOK, output)
+}
