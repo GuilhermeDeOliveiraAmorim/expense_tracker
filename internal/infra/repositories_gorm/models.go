1ab4764f5ab3f3f91aeb7427761b87a438961a9c
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-09-12 16:03:13 -0300

    feat: add many2many relationship between tag and expense

--- internal/infra/repositories_gorm/models.go
+++ internal/infra/repositories_gorm/models.go
@@ -28,7 +28,20 @@ type Expenses struct {
 	CategoryID    string     `gorm:"not null"`
 	Notes         string     `gorm:"null"`
 	Category      Categories `gorm:"foreignKey:CategoryID"`
+	Tags          []Tags     `gorm:"many2many:expense_tags"`
 	User          Users      `gorm:"foreignKey:UserID"`
+}
+
+type Tags struct {
+	ID            string    `gorm:"primaryKey;not null"`
+	Active        bool      `gorm:"not null"`
+	CreatedAt     time.Time `gorm:"not null"`
+	UpdatedAt     time.Time `gorm:"not null"`
+	DeactivatedAt time.Time `gorm:"not null"`
+	UserID        string    `gorm:"not null"`
+	Name          string    `gorm:"not null"`
+	Color         string    `gorm:"not null"`
+	User          Users     `gorm:"foreignKey:UserID"`
 }
 
 type Users struct {
