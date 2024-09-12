33a2baefb7a18b07323a93fe32c8a2d7298f8d17
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-09-12 15:45:10 -0300

    feat: add tag repository

---
+++ internal/repositories/repository_interface_tag.go
@@ -0,0 +1,10 @@
+package repositories
+
+import "github.com/GuilhermeDeOliveiraAmorim/expense-tracker/internal/entities"
+
+type TagRepositoryInterface interface {
+	CreateTag(tag entities.Tag) error
+	DeleteTag(tag entities.Tag) error
+	GetCategories(userID string) ([]entities.Tag, error)
+	GetTag(userID string, tagID string) (entities.Tag, error)
+}
